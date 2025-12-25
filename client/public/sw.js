c972cb5d6d70a811eb179370e1ec0a6e1fb3debb
alvarops11 <alvaroperezsalvador@gmail.com>
2025-12-25 22:02:42 +0100

    Fix sw.js zoneId to match ads tag

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10375759
+    "domain": "quge5.com",
+    "zoneId": 196146
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://quge5.com/act/files/service-worker.min.js?r=sw')
