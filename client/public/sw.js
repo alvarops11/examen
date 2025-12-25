6f8c1ffa6e30abec5dad62aa59046dac4aedaa9f
alvarops11 <alvaroperezsalvador@gmail.com>
2025-12-25 22:17:09 +0100

    Correct sw.js configuration for ads

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "quge5.com",
-    "zoneId": 196146
+    "domain": "3nbf4.com",
+    "zoneId": 10375759
 }
 self.lary = ""
-importScripts('https://quge5.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
