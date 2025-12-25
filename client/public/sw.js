80652345bd83dcea33d2f101420e25754c5f0ef0
alvarops11 <alvaroperezsalvador@gmail.com>
2025-12-25 22:32:23 +0100

    Configure single zone ads (Cheerful tag 10375873) and remove multitags

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10375868
+    "zoneId": 10375873
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
